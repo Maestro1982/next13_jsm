fb0e3b8bf81b6b2e748f7539319db96562dfe07d
Maestro1982 <kris.tote@telenet.be>
2023-10-06 15:23:16 +0200

    setup nextjs, folder structure, create Navbar.tsx, Footer.tsx, SearchForm.tsx and Filters.tsx in components folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+export default RootLayout;
